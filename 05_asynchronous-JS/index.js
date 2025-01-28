7c5635e55a0bdd122db48eb1696c31745b58edc2
ABHIJIT\gadak <gadakhabhijit@gmail.com>
2025-01-28 23:07:24 +0530

    section 5 complete

---
+++ 05_asynchronous-JS/index.js
@@ -0,0 +1,74 @@
+// https://dog.ceo/api/breeds/image/random
+
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePromise = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('Unable to find a file');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Unable to find a file');
+      resolve('success');
+    });
+  });
+};
+
+// readFilePromise(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body);
+
+//     return writeFilePromise('dog-img.txt', res.body.message);
+//   })
+//   .then(() => {
+//     console.log('Random dog image saved to file!');
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePromise(`${__dirname}/dog.txt`);
+    console.log('breed: ' + data);
+
+    const res1Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+
+    const imgs = all.map((el) => el.body.message);
+    console.log('imgs:');
+    console.log(imgs);
+
+    await writeFilePromise('dog-img.txt', imgs.join('\n'));
+
+    console.log('Random dog image saved to file!');
+  } catch (err) {
+    console.log(err);
+  }
+  return 'hello 2';
+};
+
+console.log('hello 1');
+getDogPic().then((x) => console.log(x));
+console.log('hello 3');
