d3cb9aa395613b353b15578412426d3197f0b6e6
ABHIJIT\gadak <gadakhabhijit@gmail.com>
2025-02-09 03:24:32 +0530

    Authentication with JWT completed

--- 10_authentication_authorization_security/models/userModel.js
+++ 10_authentication_authorization_security/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const { isLowercase } = require('validator');
 const validator = require('validator');
 
@@ -26,7 +27,23 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please confirm your name.'],
     minLength: 8,
+    validate: {
+      // will only work on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
